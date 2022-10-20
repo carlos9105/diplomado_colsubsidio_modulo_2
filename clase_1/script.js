044c31611b8830a0e1736df479115ce24e2467eb
carlos9105 <113561657+carlos9105@users.noreply.github.com>
2022-10-19 21:35:27 -0500

    actualizacion

---
+++ clase_1/script.js
@@ -0,0 +1,37 @@
+/*function test(){
+    let x=["C is fun", "Python is cool", "Javascript is amazing"]
+
+    console.log (x[0])
+    console.log (x[1])
+    console.log (x[2])
+
+}*/
+
+/*function test(x){
+    console.log (typeof(x))
+    if (x === undefined){
+        console.log ("argument not found")
+    } else {
+        console.log ("argument found")
+    }
+}*/
+
+/*function test(x){
+    if (x === undefined){
+        console.log ("argument not found")
+    } else {
+        console.log (x)
+    }
+}*/
+
+/*function test(x, y){
+    console.log(x+" is "+y)
+}*/
+
+function test(x){
+    if (typeof x === "number"){
+        console.log (`Your number is ${x}`)
+    } else {
+        console.log ("Not a number")
+    }
+}
